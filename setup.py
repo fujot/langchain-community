6c49af1b044f005142e35990679b9981fda150bc
user <user@usernoMacBook-Pro.local>
2025-05-11 23:42:24 +0900

    add setup.py/__init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="langchain-community",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="LangChain Community integrations.",
+    author="fujot",
+    url="https://github.com/fujot/langchain-community",
+)
